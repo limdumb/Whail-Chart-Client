6fe65373bf7e22b1254a280d555b2acc98597509
limdumb <dlaruddls@nate.com>
2023-08-15 19:25:12 +0900

    [MISC] PageView File 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <BrowserRouter>
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
